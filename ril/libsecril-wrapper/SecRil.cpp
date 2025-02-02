b15bd825bfa3843a22094b78f9a77ed8dcb2bbf7
LuK1337 <priv.luk@gmail.com>
2025-02-02 13:05:34 +0100

    meme
    Change-Id: I37e196cc622ff3e5891207217cd51f5635465196

---
+++ ril/libsecril-wrapper/SecRil.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dlfcn.h>
+
+extern "C" int _ZN6SecRil15RequestCompleteEP7RequestiP7RilDatab(void* thisptr, void* request,
+                                                                int msg, void* rilData, bool unk) {
+    static auto orig = reinterpret_cast<typeof(_ZN6SecRil15RequestCompleteEP7RequestiP7RilDatab)*>(
+            dlsym(RTLD_NEXT, "_ZN6SecRil15RequestCompleteEP7RequestiP7RilDatab"));
+
+    // ~ some nice comment describing why goes here ~
+    if (msg == 100) {
+        rilData = nullptr;
+    }
+
+    return orig(thisptr, request, msg, rilData, unk);
+}
+
+extern "C" void* RIL_Init(void* env, int argc, char** argv) {
+    static auto orig = reinterpret_cast<typeof(RIL_Init)*>(
+            dlsym(dlopen("libsec-ril.so", RTLD_NOW), "RIL_Init"));
+    return orig(env, argc, argv);
+}
